b163f002d8e3ff5f3c6417acae256ef5f36301d9
Marvin Kosmider <marvin.kosmider@technikum-wien.at>
2025-05-07 15:34:15 +0200

    inital commit

---
+++ src/main/java/at/technikum/todoapp/controller/TodoController.java
@@ -0,0 +1,40 @@
+package at.technikum.todoapp.controller;
+
+import at.technikum.todoapp.entity.Todo;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+
+    @GetMapping
+    public List<Todo> readAll() {
+        return null;
+    }
+
+    @GetMapping("/{id}")
+    public Todo read(@PathVariable String id) {
+        return null;
+    }
+
+    @PostMapping
+    public Todo create(@RequestBody Todo todo) {
+        return null;
+    }
+
+    @PutMapping("/{id}")
+    public Todo update(
+            @PathVariable String id,
+            @RequestBody Todo todo
+    ) {
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public Todo delete(@PathVariable String id) {
+        return null;
+    }
+}
---
+++ src/main/java/at/technikum/todoapp/entity/Todo.java
@@ -0,0 +1,50 @@
+package at.technikum.todoapp.entity;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class Todo {
+
+    private String id;
+
+    private String task;
+
+    private LocalDateTime due;
+
+    private boolean done;
+
+    public Todo(String task, LocalDateTime due, boolean done) {
+        this.id = UUID.randomUUID().toString();
+        this.task = task;
+        this.due = due;
+        this.done = done;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    public LocalDateTime getDue() {
+        return due;
+    }
+
+    public void setDue(LocalDateTime due) {
+        this.due = due;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+}
